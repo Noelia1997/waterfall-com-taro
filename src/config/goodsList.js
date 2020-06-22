0734aad3b8e3a88de95f8f85eb3813426e805941
卢丽蓉Noelia <you@example.com>
2020-06-22 22:19:15 +0800

    'over'

---
+++ src/config/goodsList.js
@@ -0,0 +1,110 @@
+const img1 = require('../images/touxiang.jpg')
+const img2 = require('../images/touxiang2.jpg')
+
+const goodsList =
+  [
+    {
+      image: img1 ,
+      id:0,
+      value:'1'
+    },
+    {
+      image: img1,
+      id:1,
+      value:'2'
+    },
+    {
+      image: img2,
+      id:2,
+      value:'3'
+    },
+    {
+      image: img1,
+      id:3,
+      value:'4'
+    },
+    {
+      image: img1 ,
+      id:4,
+      value:'5'
+    },
+    {
+      image: img1,
+      id:5,
+      value:'6'
+    },
+    {
+      image: img1 ,
+      id:6,
+      value:'7'
+    },
+    {
+      image: img2,
+      id:7,
+      value:'8'
+    },
+    {
+      image: img2,
+      id:8,
+      value:'9'
+    },
+    {
+      image: img1,
+      id:9,
+      value:'10'
+    },
+    {
+      image: img1,
+      id:10,
+      value:'11'
+    },
+    {
+      image: img2,
+      id:11,
+      value:'12'
+    },
+    {
+      image: img2,
+      id:12,
+      value:'13'
+    },
+    {
+      image: img1,
+      id:13,
+      value:'14'
+    },
+    {
+      image: img1,
+      id:14,
+      value:'15'
+    },
+
+
+    // {
+    //   image: require('../images/touxiang.jpg'),
+    //   value: 'right'
+    // },
+    // {
+    //   image: require('../images/touxiang2.jpg'),
+    //   value: 'left'
+    // },
+    // {
+    //   image: require('../images/touxiang.jpg'),
+    //   value: 'right'
+    // },
+    // {
+    //   image: require('../images/touxiang2.jpg'),
+    //   value: 'left'
+    // },
+    // {
+    //   image: require('../images/touxiang.jpg'),
+    //   value: 'right'
+    // },
+    // {
+    //   image: require('../images/touxiang2.jpg'),
+    //   value: 'left'
+    // },
+
+  ]
+
+export default goodsList
